81c2a613e6b0484c9cc503e4384957f0f8a252a7
citixenken <kmuyesu@gmail.com>
2022-02-15 22:19:13 +0300

    showing multiple options with confirmationDialog

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,28 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount = 0.0
+//    @State private var blurAmount = 0.0
+    
     //    {
     //        didSet {
     //            print("New value is \(blurAmount)")
     //        }
     //    }
     
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.white
+    
     var body: some View {
-        VStack {
-            Text("Hello, world!")
-                .blur(radius: blurAmount)
-            
-            
-            Slider(value: $blurAmount, in: 0...10)
-                .onChange(of: blurAmount) { newValue in
-                    print("New value is \(newValue)")
-                }
-            
-            //            Button("Random Blur") {
-            //                blurAmount = Double.random(in: 1...10)
-        }
-    }
+        Text("Hello, world!")
+            .frame(width: 300, height: 300)
+            .background(backgroundColor)
+            .onTapGesture {
+                showingConfirmation = true
+            }
+        
+            .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+                Button("Red") { backgroundColor = .red }
+                Button("Green") { backgroundColor = .green }
+                Button("Blue") { backgroundColor = .blue }
+                Button("Cancel", role: .cancel) { }
+            } message: {
+                Text("Select a new color")
+            }
+//        VStack {
+//            Text("Hello, world!")
+//                .blur(radius: blurAmount)
+//
+//
+//            Slider(value: $blurAmount, in: 0...10)
+//                .onChange(of: blurAmount) { newValue in
+//                    print("New value is \(newValue)")
+//                }
+//
+//                        Button("Random Blur") {
+//                            blurAmount = Double.random(in: 1...10)
+//        }
+  }
 }
 
 
