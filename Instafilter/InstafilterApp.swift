6919510c463c72464554222ad24562abc3f8a24a
citixenken <kmuyesu@gmail.com>
2022-02-15 14:36:19 +0300

    initial commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Ken Muyesu on 15/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
